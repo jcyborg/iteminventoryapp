19e6032c069263e649eceea26ddc344867629e84
Jubril Bakare <jubril@thecodebridge.com>
2025-01-16 05:02:26 -0500

    prod

--- dist/inventory-app/server/main.server.mjs
+++ dist/inventory-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BSZVDTVE.mjs";import"./chunk-4ZNQALZB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HPHUCYRZ.mjs";import"./chunk-NSSPSOVG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
